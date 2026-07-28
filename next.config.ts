b92c9c02e14e21ffae04f3d572ab1623632ce15f
trantuyetthaosharp <trantuyetthaosharp@gmail.com>
2026-07-28 10:31:44 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb', // Tăng giới hạn lên 10MB
+    },
+  },
+};
 
 export default nextConfig;
