0b0ac6268e8ed325f3407f1523795b3c39dcef27
nguyenanvuong2006 <nguyenanvuong2006@gmail.com>
2026-08-16 05:26:51 +0700

    style(document-merge): scope parity theme to merge center

---
+++ src/app/(internal)/admin/document-merge/layout.tsx
@@ -0,0 +1,5 @@
+import "./document-merge-parity.css";
+
+export default function DocumentMergeLayout({ children }: { children: React.ReactNode }) {
+  return <div className="document-merge-parity">{children}</div>;
+}
